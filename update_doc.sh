9cfbdaa8f6c26c4470c25c54c4da2e1b1a2684f0
Remington Campbell <remcampb@cisco.com>
2018-11-20 16:01:10 -0800

    Add update_doc.sh

---
+++ update_doc.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+docker build -t tdm/doc doc/
+COMPOSE_FILE=""
+case "$1" in
+    ""|"http")
+        COMPOSE_FILE=docker-compose.yml
+        ;;
+    "https")
+        COMPOSE_FILE=docker-compose.https.yml
+        ;;
+    *)
+        echo $"Usage: $0 [http|https]"
+        exit 1
+esac
+docker-compose -f $COMPOSE_FILE up -d --no-deps doc
