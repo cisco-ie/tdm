4c3ac370fd1b969975cfff76adc20d4539d8c96c
Remington Campbell <remcampb@cisco.com>
2018-12-05 13:01:22 -0800

    Fix standalone vuepress

--- doc/standalone.sh
+++ doc/standalone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t tdm/doc_test .
-docker run -d -p 8089:8080 -v $(pwd)/docs:/data/docs:rw --name tdm_dev_docs tdm/doc_test run vuepress dev docs
+docker run -d -p 8089:8080 -v $(pwd)/docs:/data/docs:rw --name tdm_dev_docs tdm/doc_test docs:dev
