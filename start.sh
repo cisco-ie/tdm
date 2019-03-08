2ca133cc60ec4da2845fc3b083c84160c3536ade
Remington Campbell <remcampb@cisco.com>
2019-03-07 16:29:47 -0800

    Fix docker-compose func break

--- start.sh
+++ start.sh
@@ -1,29 +1,13 @@
 #!/usr/bin/env bash
 function check_sysctl() {
-    ES_RECC_MIN_MAX_MAP_COUNT=262144
+    ES_VM_MAX_MAP_COUNT_MIN=262144
     ES_VM_MAX_MAP_COUNT=$(sysctl -n vm.max_map_count)
-    if [ "$ES_VM_MAX_MAP_COUNT" -lt "$ES_RECC_MIN_MAX_MAP_COUNT" ]; then
-        echo "vm.max_map_count=$ES_VM_MAX_MAP_COUNT is below ElasticSearch's recommended $ES_RECC_MIN_MAX_MAP_COUNT."
-        echo "Recommend running \"sysctl -w vm.max_map_count=$ES_RECC_MIN_MAX_MAP_COUNT\""
-        echo "Continuing will still work, but against recommended settings."
+    if [ "$ES_VM_MAX_MAP_COUNT" -lt "$ES_VM_MAX_MAP_COUNT_MIN" ]; then
+        echo "vm.max_map_count=$ES_VM_MAX_MAP_COUNT is below ElasticSearch's recommended $ES_VM_MAX_MAP_COUNT_MIN."
+        echo "Recommend running \"sysctl -w vm.max_map_count=$ES_VM_MAX_MAP_COUNT_MIN\""
+        echo "The system will still function, but against recommended settings."
         read -p "Press [Enter] to continue."
     fi
-}
-
-function docker_up() {
-    COMPOSE_FILE=""
-    case "$1" in
-        ""|"http")
-            COMPOSE_FILE=docker-compose.yml
-            ;;
-        "https")
-            COMPOSE_FILE=docker-compose.https.yml
-            ;;
-        *)
-            echo $"Usage: $0 [http|https]"
-            exit 1
-    esac
-    docker-compose -f $COMPOSE_FILE up -d --build
 }
 
 UNAME="$(uname -s)"
@@ -35,5 +19,19 @@ case "${UNAME}" in
         echo "Recommend running on Linux for production purposes."
         echo "YMMV on Windows or MacOS."
         sleep 5s
+        ;;
 esac
-docker_up
+COMPOSE_FILE=""
+case "$1" in
+    ""|"http")
+        COMPOSE_FILE=docker-compose.yml
+        ;;
+    "https")
+        COMPOSE_FILE=docker-compose.https.yml
+        ;;
+    *)
+        echo $"Usage: $0 [http|https]"
+        exit 1
+        ;;
+esac
+docker-compose -f $COMPOSE_FILE up -d --build
